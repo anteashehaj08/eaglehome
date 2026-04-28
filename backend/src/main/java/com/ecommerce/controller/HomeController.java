398f38e976fc78b47e339a3cbb027de7660fcda0
Antea Shehaj <anteashehaj08@hotmail.com>
2026-04-28 14:01:16 +0200

    fix: add simple root endpoint to verify backend is running and help debug deployment issues

--- backend/src/main/java/com/ecommerce/controller/HomeController.java
+++ backend/src/main/java/com/ecommerce/controller/HomeController.java
@@ -1,4 +1,8 @@
 package com.ecommerce.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class HomeController {
         @GetMapping("/")
